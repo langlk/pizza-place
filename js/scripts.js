d6e022c071902ea4d96ecb7c5a3d847ea1133f41
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-25 08:57:05 -0700

    Add JS Pizza object to store a pizza's information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+// Business Logic
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+// UI Logic
 $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
     var size = $("select#size").val();
     var toppings = $("input#toppings").val().split(",");
-    console.log("Size: " + size);
-    console.log("Toppings: " + toppings);
+
+    var pizzaOrder = new Pizza(size, toppings);
+    $(".order-confirm").append("<p class='pizza-order'>"+size+" Pizza</p>");
+    $(".pizza-order").last().click(function() {
+      console.log(pizzaOrder);
+    });
   });
 });
